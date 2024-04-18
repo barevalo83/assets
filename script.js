39f57395ef0f1ac3fc6fb773afbe731f3187532b
Brian Arevalo <78184275+barevalo83@users.noreply.github.com>
2024-04-17 21:05:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Función para desplazarse suavemente al principio de la página
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+function openLinkedIn() {
+    window.open('https://www.linkedin.com/in/barevalo83', '_blank');
+}
+
